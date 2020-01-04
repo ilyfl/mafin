703238074590ac9dfa039650c2434e9d7ac1c2f9
ilyfl <artempolishch@gmail.com>
2020-01-04 17:39:40 +0200

    store.c

---
+++ store.c
@@ -0,0 +1,7 @@
+
+//fin.c collects the information from the user and sends it in this place
+//this file gets this information and stores it into file specificated by config file
+int main(int argc, char**argv)
+{
+
+}
