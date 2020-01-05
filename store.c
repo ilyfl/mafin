9545870ba48bb137ee492f23f405a9e3e17a3a27
ilyfl <artempolishch@gmail.com>
2020-01-05 17:06:08 +0200

    a function declaration created

--- store.c
+++ store.c
@@ -1,7 +1,11 @@
 
 //fin.c collects the information from the user and sends it in this place
 //this file gets this information and stores it into file specificated by config file
-int main(int argc, char**argv)
-{
 
+#include fin.h
+
+int store(answer_t info)
+{
+		
+	return 0;
 }
