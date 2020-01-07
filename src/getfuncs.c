b08d066bfdea3c7f4ddf539697df4525fc0690e2
ilyfl <artempolishch@gmail.com>
2020-01-07 22:47:12 +0200

    get_* functions separated

---
+++ src/getfuncs.c
@@ -0,0 +1,81 @@
+#include "fin.h"
+
+uint8_t get_str(char str[])
+{
+	char c=getchar();
+	uint8_t i=0;
+	do {
+		if(c=='\n')
+		{
+			return 1;
+		}
+		else
+		{
+			str[i]=c;	
+		}
+		++i;
+	} while((c=getchar())!='\n');
+	str[i]='\0';
+	return 0;
+}
+
+uint8_t get_float(float *number){
+	char buffer[64];
+    	char *endptr;
+	while(get_str(buffer))
+		;
+	
+   	//if (fgets(buffer, sizeof(buffer), stdin) == NULL)
+   	//    	return 1; /* Unexpected error */
+   	*number = strtof(buffer, &endptr);
+   	if ((*endptr == '\0') || (isspace(*endptr) != 0))
+   	    	return 0;
+   	else
+		return 1;
+}
+
+void get_digit(int* digit)
+{
+	unsigned char c;
+	while(c=getchar())
+		if(isdigit(c))
+			break;
+		else
+			continue;
+	*digit=c-'0';
+}
+
+uint8_t get_name(char *name){
+	if(fgets(name, sizeof(name), stdin))
+	{
+		return 1;
+	}
+	
+	uint8_t len = strlen(name)-1;	
+	name[len]='\0';
+	return 0;
+}
+
+uint8_t get_username(char **name)
+{
+	*name=getenv("USER");
+	if(*name==NULL) return 1;
+	return 0;
+}
+
+//TODO: no echo input
+uint8_t get_password(char *password)
+{
+	fgets(password, sizeof(password), stdin);
+
+	uint8_t len = strlen(password)-1;	
+	password[len]='\0';
+
+	if((strlen(password) < MIN_INP_LEN))
+	{
+		printf("Password has to contain up to %d and more than %d characters!\n",NAME_MAX, MIN_INP_LEN);
+		return 1;
+	}
+	return 0;
+}
+
