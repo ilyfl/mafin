5442ef3f232d712fa0237005aaecc72a30ce2d2a
ilyfl <artempolishch@gmail.com>
2020-01-22 16:23:25 +0200

    math.h added

--- src/misc.h
+++ src/misc.h
@@ -1,5 +1,5 @@
 #pragma once
-void get_digit(uint8_t *digit);
+void get_digit(uint8_t *digit, uint8_t left, uint8_t right);
 uint8_t get_str(char str[]);
 uint8_t get_float(float *number);
 uint8_t get_password(char* password);
