9e8f9057c4fe8323f018aac92e35a77acbf3357b
ilyfl <artempolishch@gmail.com>
2020-01-22 16:23:34 +0200

    storedb storing format changed, readdb works

--- src/stream.c
+++ src/stream.c
@@ -3,6 +3,7 @@
 
 #include "fin.h"
 #include "stream.h"
+#include "math.h"
 
 
 //currently writes on the first line only
@@ -72,17 +73,12 @@ uint8_t readdb(char* dbpath, answer_t* info)
 	int* ptr = &info->time.tm_sec;
 	memset(ptr, 0, sizeof(struct tm));
 
-	ptr = &info->time.tm_mday;	
 	for(uint8_t i=0; i<sizeof(timeline);++i)
 	{
 		if(isdigit(timeline[i]))
 			*ptr=10*(*ptr)+timeline[i]-'0';		
-		else if(timeline[i]=='-')
-			ptr++;
-		else if(timeline[i]=='/')
-			ptr=&info->time.tm_hour;
-		else if(timeline[i]==':')
-			ptr--;
+		else if(ispunct(timeline[i]))
+			++ptr;
 		else
 			continue;
 	}	
@@ -94,28 +90,56 @@ uint8_t readdb(char* dbpath, answer_t* info)
 
 //not working properly	
 //need string to float converter 
-//identifies '-' as digit
-	uint8_t* uiptr = &info->typecome;
-	float * fptr = &info->payload;
-	*fptr=0;
-	memset(uiptr, 0, 64);
-	char* cptr = info->comment;
+
+	//initializing by zeros
+	char* cptr=&info->comment;
+	memset(cptr, 0, 64);
+	info->payload=0;
+	info->tcr=0;
+
+	int8_t flag=0;
+	int8_t a=1;
+	int8_t e=0;
+	//parser
 	for(uint8_t i=0; i<strlen(infoline);++i){
-		if(isdigit(infoline[i]))
+		while(isdigit(infoline[i]) && !flag)
 		{
-			*uiptr=10*(*uiptr)+infoline[i]-'0';
-			uiptr++;
+			info->tcr=10*info->tcr+infoline[i]-'0';
+			++i;
 		}
-		else if(ispunct(infoline[i]))
-			continue;
-		else if(isalnum(infoline[i]))
+		while(isdigit(infoline[i]) && flag==1)
 		{
-			*uiptr=infoline[i];
-			uiptr++;	
+			info->payload=10*info->payload+infoline[i]-'0';
+			++i;	
 		}
-
-
+		while(isalnum(infoline[i])&& flag==2)
+		{
+			*cptr++=infoline[i];	
+			++i;
+		}
+		if(infoline[i]=='.')
+		{
+			++i;
+			while(isdigit(infoline[i]))
+			{
+				info->payload=10*info->payload+infoline[i]-'0';	
+				e-=1;
+				++i;
+			}
+		}
+		else if(infoline[i]=='-')
+			a=-1;
+		if(infoline[i]==',')
+			flag++;
 	}
+
+	info->tcr*=a;
+	while(e)
+	{
+		info->payload*=0.1;
+		++e;
+	}
+	roundf(info->payload);
 	
 	fclose(dbfd);
 	return 0;
@@ -132,8 +156,8 @@ uint8_t storedb(answer_t* info, char* dbpath)
 	{	
 		return 1;	
 	}	
-	fprintf(db,"%02d-%02d-%d/%02d:%02d:%02d|",info->time.tm_mday, info->time.tm_mon+1, info->time.tm_year+1900, info->time.tm_hour, info->time.tm_min, info->time.tm_sec);	
-	fprintf(db,"%d,%d,%d,%f,%s\n", info->typecome, info->category, info->resource, info->payload, info->comment);
+	fprintf(db,"%02d-%02d-%02d/%02d:%02d:%d|",info->time.tm_sec, info->time.tm_min, info->time.tm_hour, info->time.tm_mday, info->time.tm_mon, info->time.tm_year);	
+	fprintf(db,"%d,%.2f,%s\n",info->tcr, roundf(info->payload), info->comment);
 
 	fclose(db);
 	
