6562843be55f18459f05b6ef888d29f32cbae5dc
ilyfl <artempolishch@gmail.com>
2020-01-31 14:29:17 +0200

    Declaration changes

--- src/misc.h
+++ src/misc.h
@@ -1,7 +1,7 @@
 #pragma once
-void get_digit(uint8_t *digit, uint8_t left, uint8_t right);
-uint8_t get_str(char str[]);
-uint8_t get_float(float *number);
+void get_digit(FILE* stream,uint8_t *digit, uint8_t left, uint8_t right);
+uint8_t get_str(FILE* stream, char str[]);
+uint8_t get_float(FILE* stream, float *number);
 uint8_t get_password(char* password);
 uint8_t get_name(char *name);
 uint8_t get_username(char **name);
